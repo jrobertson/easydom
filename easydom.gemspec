4224da35edcb735cd467a294e61e3bab7d423b36
James Robertson <james@jamesrobertson.eu>
2020-05-02 17:36:43 +0100

    feature: The DOM can now be built from scratch, just using missing methods as element names.

--- easydom.gemspec
+++ easydom.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'easydom'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Experimental gem to access XML elements from simple dot notation (e.g. person.name).'
   s.authors = ['James Robertson']
   s.files = Dir['lib/easydom.rb']
-  s.add_runtime_dependency('free_dom', '~> 0.1', '>=0.1.0')
+  s.add_runtime_dependency('free_dom', '~> 0.3', '>=0.3.1')
   s.signing_key = '../privatekeys/easydom.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
--- lib/easydom.rb
+++ lib/easydom.rb
@@ -7,12 +7,22 @@ require 'free_dom'
 
 class EasyDom
 
-  def initialize(obj, debug: false)
+  def initialize(obj=nil, debug: false, root: 'root')
 
     @debug = debug
+    
+    obj ||= "<#{root}/>"
 
     @e = obj.is_a?(String) ? FreeDom.new(obj).root : obj      
 
+  end
+  
+  def ==(v)
+    @e.attributes.values.first.to_s == v
+  end  
+  
+  def to_s()
+    @e.attributes.values.first.to_s
   end
 
   def method_missing(sym, *args, &block)
@@ -20,11 +30,49 @@ class EasyDom
     puts "name: %s; args: %s" % [sym, args.inspect] if @debug
 
     if @e.respond_to? sym then
+      
       puts 'attribute found' if @debug
       @e.method(sym).call(*args)
+      
+    elsif sym.to_s[-1] == '='
+      
+      # assumes the 1st attribute it finds is the attribute to be set
+      
+      puts '==' if @debug
+      e = @e.element(sym.to_s[0..-2])
+      
+      if e.nil? then
+
+        attribute = sym[0..-2]
+        
+        @e.instance_eval("def #{attribute}()
+          attributes[:#{attribute}]
+        end")
+        
+        @e.instance_eval("def #{sym}(val)
+          attributes[:#{attribute}] = val
+          val
+        end")
+        
+        return @e.method(sym).call(args.first)
+      end
+      
+      attr = e.attributes.keys.first.to_s        
+      
+      puts 'attribute: ' + attr.inspect if @debug
+      
+      e.method((attr + '=').to_sym).call(*args)
+      
     else
+      
+      puts 'query element: ' + sym.to_s if @debug
       e = @e.element(sym.to_s)
+      
+      # if the element doesn't exist, create it
+      e = @e.add FreeDom::Element.new(sym, attributes: {}) unless e
+      
       EasyDom.new(e, debug: @debug) if e
+      
     end
 
   end
